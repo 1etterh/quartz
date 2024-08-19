367057dc8eb155c1b4582d9118bb26aa51cedcfc
Seohyun Kim <153896425+1etterh@users.noreply.github.com>
2024-08-19 13:56:32 +0900

    Quartz sync: Aug 19, 2024, 1:56 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "1etterh.github.io.devLog",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
